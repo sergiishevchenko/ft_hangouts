3bfde73371dea91b47ad927abb6b7427f9eda6e4
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-10-18 22:24:05 +0200

    set default values

--- app/src/main/java/com/example/ft_hangouts_42/model/Contact.kt
+++ app/src/main/java/com/example/ft_hangouts_42/model/Contact.kt
@@ -4,8 +4,8 @@ data class Contact(
     var id: Long = 0L,
     var name: String,
     var phone: String,
-    var email: String?,
-    var address: String?,
-    var notes: String?,
+    var email: String? = null,
+    var address: String? = null,
+    var notes: String? = null,
     var avatarPath: String? = null
 )

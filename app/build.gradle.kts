1009bc6ebf8fcb6c21c036f1d899c208085111ad
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-11-14 17:46:55 +0100

    add lifecycle-viewmodel-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.4.0")
     implementation("androidx.compose.material:material-icons-extended:1.5.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
     implementation("io.coil-kt:coil-compose:2.6.0")
 
     // Room
